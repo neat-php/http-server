c3d94840bba2a65779efd969933b8336e5f48021
Guido van Biemen <gvanbiemen@shopbox.nl>
2019-12-11 19:14:53 +0100

    Add Responder.

---
+++ classes/Responder.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Neat\Http\Server;
+
+use Generator;
+use Neat\Http\Response;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+
+class Responder
+{
+    /** @var ResponseFactoryInterface */
+    private $responseFactory;
+
+    /** @var StreamFactoryInterface */
+    private $streamFactory;
+
+    /** @var callable[] */
+    private $factories;
+
+    /**
+     * Responder constructor
+     *
+     * @param ResponseFactoryInterface $responseFactory
+     * @param StreamFactoryInterface   $streamFactory
+     */
+    public function __construct(ResponseFactoryInterface $responseFactory, StreamFactoryInterface $streamFactory)
+    {
+        $this->responseFactory = $responseFactory;
+        $this->streamFactory   = $streamFactory;
+
+        $this->register(Response::class, function (Response $response) {
+            return $response;
+        });
+    }
+
+    /**
+     * @param $output
+     * @return Generator|string[]
+     */
+    private function types($output)
+    {
+        if (is_object($output)) {
+            yield get_class($output);
+            yield from class_parents($output);
+            yield from class_implements($output);
+        }
+
+        yield gettype($output);
+    }
+
+    /**
+     * @param string   $type
+     * @param callable $factory
+     */
+    public function register(string $type, callable $factory)
+    {
+        $this->factories[$type] = $factory;
+    }
+
+    /**
+     * @param mixed $output
+     * @return Response
+     */
+    public function resolve($output): Response
+    {
+        foreach ($this->types($output) as $type) {
+            if ($factory = $this->factories[$type] ?? null) {
+                return $factory($output);
+            }
+        }
+
+        return $this->json($output);
+    }
+
+    /**
+     * @param string $html
+     * @return Response
+     */
+    public function html(string $html): Response
+    {
+        return $this->response()
+            ->withContentType('text/html')
+            ->withBody($this->streamFactory->createStream($html));
+    }
+
+    /**
+     * @param mixed $data
+     * @return Response
+     */
+    public function json($data): Response
+    {
+        return $this->response()
+            ->withContentType('application/json')
+            ->withBody($this->streamFactory->createStream(json_encode($data)));
+    }
+
+    /**
+     * @param string $text
+     * @return Response
+     */
+    public function text(string $text)
+    {
+        return $this->response()
+            ->withContentType('text/plain')
+            ->withBody($this->streamFactory->createStream($text));
+    }
+
+    /**
+     * @return Response
+     */
+    public function response()
+    {
+        return new Response($this->responseFactory->createResponse());
+    }
+}
---
+++ tests/ResponderTest.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace Neat\Http\Server\Test;
+
+use Neat\Http\Response;
+use Neat\Http\Server\Responder;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\StreamInterface;
+use stdClass;
+
+class ResponderTest extends TestCase
+{
+    public function testResolveResponse()
+    {
+        $responder = new Responder(
+            $responseFactory = $this->createMock(ResponseFactoryInterface::class),
+            $streamFactory = $this->createMock(StreamFactoryInterface::class)
+        );
+
+        $response = $this->createMock(Response::class);
+
+        $this->assertSame($response, $responder->resolve($response));
+    }
+
+    public function testResolveClass()
+    {
+        $object = new stdClass();
+
+        $response = $this->createMock(Response::class);
+
+        $factory = $this->createMock(CallableMock::class);
+        $factory->expects($this->once())->method('__invoke')->with($object)->willReturn($response);
+
+        $responder = new Responder(
+            $responseFactory = $this->createMock(ResponseFactoryInterface::class),
+            $streamFactory = $this->createMock(StreamFactoryInterface::class)
+        );
+        $responder->register(stdClass::class, $factory);
+
+        $this->assertSame($response, $responder->resolve($object));
+    }
+
+    public function testResolveInterface()
+    {
+        $psrResponse = $this->createMock(ResponseInterface::class);
+
+        $response = $this->createMock(Response::class);
+
+        $factory = $this->createMock(CallableMock::class);
+        $factory->expects($this->once())->method('__invoke')->with($psrResponse)->willReturn($response);
+
+        $responder = new Responder(
+            $responseFactory = $this->createMock(ResponseFactoryInterface::class),
+            $streamFactory = $this->createMock(StreamFactoryInterface::class)
+        );
+
+        $responder->register(ResponseInterface::class, $factory);
+
+        $this->assertSame($response, $responder->resolve($psrResponse));
+    }
+
+    public function testResolveObject()
+    {
+        $object = new stdClass();
+
+        $response = $this->createMock(Response::class);
+
+        $factory = $this->createMock(CallableMock::class);
+        $factory->expects($this->once())->method('__invoke')->with($object)->willReturn($response);
+
+        $responder = new Responder(
+            $responseFactory = $this->createMock(ResponseFactoryInterface::class),
+            $streamFactory = $this->createMock(StreamFactoryInterface::class)
+        );
+
+        $responder->register('object', $factory);
+
+        $this->assertSame($response, $responder->resolve($object));
+    }
+
+    public function testResolveType()
+    {
+        $string = 'testing';
+
+        $response = $this->createMock(Response::class);
+
+        $factory = $this->createMock(CallableMock::class);
+        $factory->expects($this->once())->method('__invoke')->with($string)->willReturn($response);
+
+        $responder = new Responder(
+            $responseFactory = $this->createMock(ResponseFactoryInterface::class),
+            $streamFactory = $this->createMock(StreamFactoryInterface::class)
+        );
+
+        $responder->register('string', $factory);
+
+        $this->assertSame($response, $responder->resolve($string));
+    }
+
+    public function testResolveDefault()
+    {
+        $response = $this->createMock(Response::class);
+        $responder = $this->createPartialMock(Responder::class, ['json']);
+        $responder->expects($this->once())->method('json')->with([])->willReturn($response);
+
+        $this->assertSame($response, $responder->resolve([]));
+    }
+
+    public function testJson()
+    {
+        $responder = new Responder(
+            $responseFactory = $this->createMock(ResponseFactoryInterface::class),
+            $streamFactory = $this->createMock(StreamFactoryInterface::class)
+        );
+
+        $stream = $this->createMock(StreamInterface::class);
+
+        $response = $this->createMock(ResponseInterface::class);
+        $response->expects($this->once())->method('withBody')->with($stream)->willReturnSelf();
+        $response->expects($this->once())->method('withHeader')->with('Content-Type', 'application/json')->willReturnSelf();
+
+        $responseFactory->expects($this->once())->method('createResponse')->with()->willReturn($response);
+
+        $streamFactory->expects($this->once())->method('createStream')->with('{"key":"value"}')->willReturn($stream);
+
+        $this->assertSame($response, $responder->json(['key' => 'value'])->psr());
+    }
+
+    public function testText()
+    {
+        $responder = new Responder(
+            $responseFactory = $this->createMock(ResponseFactoryInterface::class),
+            $streamFactory = $this->createMock(StreamFactoryInterface::class)
+        );
+
+        $stream = $this->createMock(StreamInterface::class);
+
+        $response = $this->createMock(ResponseInterface::class);
+        $response->expects($this->once())->method('withBody')->with($stream)->willReturnSelf();
+        $response->expects($this->once())->method('withHeader')->with('Content-Type', 'text/plain')->willReturnSelf();
+
+        $responseFactory->expects($this->once())->method('createResponse')->with()->willReturn($response);
+
+        $streamFactory->expects($this->once())->method('createStream')->with('Hello world!')->willReturn($stream);
+
+        $this->assertSame($response, $responder->text('Hello world!')->psr());
+    }
+
+    public function testHtml()
+    {
+        $responder = new Responder(
+            $responseFactory = $this->createMock(ResponseFactoryInterface::class),
+            $streamFactory = $this->createMock(StreamFactoryInterface::class)
+        );
+
+        $stream = $this->createMock(StreamInterface::class);
+
+        $response = $this->createMock(ResponseInterface::class);
+        $response->expects($this->once())->method('withBody')->with($stream)->willReturnSelf();
+        $response->expects($this->once())->method('withHeader')->with('Content-Type', 'text/html')->willReturnSelf();
+
+        $responseFactory->expects($this->once())->method('createResponse')->with()->willReturn($response);
+
+        $streamFactory->expects($this->once())->method('createStream')->with('<html lang="en"><body>Hello world!</body></html>')->willReturn($stream);
+
+        $this->assertSame($response, $responder->html('<html lang="en"><body>Hello world!</body></html>')->psr());
+    }
+}
